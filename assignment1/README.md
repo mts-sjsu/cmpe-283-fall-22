01ef2f84ea6a2ac9c4a0c0ee59771dc8315a7772
MU-TE SHEN <mute.shen@sjsu.edu>
2022-11-06 12:48:10 -0800

    First pass implementing procbased, exit, and entry controls

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,1 +1,34 @@
 # CMPE 283 Fall 2022 Assignment 1
+
+## Team
+I did this assignment on my own.
+
+
+
+
+
+
+## Steps
+1. Create a GCP instance using ubuntu 22.04 provided by GCP
+   1. Make sure to enable nested virtualization
+2. Install required modules
+  ```shell
+  > sudo apt install gcc make
+  > sudo apt-get linux-headers-$(uname -r)
+  ```
+3. Connect the GCP instance with Github
+   1. This can be achieved by adding the public key of the instance to Github repository deploy keys. Tutorial [here](https://docs.github.com/en/developers/overview/managing-deploy-keys)
+4. `git clone` the repository with ssh
+5. Verify the provided code yields expected results
+```shell
+make
+sudo insmod ./cmpe283-1.ko #insert the built module into the Linux Kernel
+sudo dmesg #print the kernel ring buffer
+```
+6. Implement the rest of the MSRs
+   1. Pinbased controls: SDM V3, 24.6.1
+   2. Procbased controls: SDM V3, 24.6.2
+   3. Exit controls: SDM V3, 24.7.1
+   4. Entry controls: SDM V3, 24.8.1
+   5. Secondary procbased controls: SDM V3, 24.6.2
+   6. Tertiary procbased controls: SDM V3, 24.6.2
--- assignment1/cmpe283-1.c
+++ assignment1/cmpe283-1.c
@@ -12,6 +12,11 @@
  * See SDM volume 4, section 2.1
  */
 #define IA32_VMX_PINBASED_CTLS	0x481
+#define IA32_VMX_PROCBASED_CTLS	0x482
+#define IA32_VMX_EXIT_CTLS	0x483
+#define IA32_VMX_ENTRY_CTLS	0x484
+#define IA32_VMX_PROCBASED_CTLS2	0x48B
+#define IA32_VMX_PROCBASED_CTLS3	0x492
 
 /*
  * struct caapability_info
@@ -36,6 +41,82 @@ struct capability_info pinbased[5] =
 	{ 5, "Virtual NMIs" },
 	{ 6, "Activate VMX Preemption Timer" },
 	{ 7, "Process Posted Interrupts" }
+};
+
+/*
+ * Procbased capabilities 1
+ * See SDM volume 3, section 24.6.2
+ */
+struct capability_info procbased[5] =
+{
+	{ 2, "Interrupt-window Exiting" },
+	{ 3, "Use TSC Offsetting" },
+	{ 7, "HLT Exiting" },
+	{ 9, "INVLPG Exiting" },
+	{ 10, "MWAIT Exiting" },
+	{ 11, "RDPMC Exiting" },
+	{ 12, "RDTSC Exiting" },
+	{ 15, "CR3-load Exiting" },
+	{ 16, "CR3-store Exiting" },
+	{ 17, "Activate Tertiary Controls" },
+	{ 19, "CR8-load Exiting" },
+	{ 20, "CR8-store Exiting" },
+	{ 21, "Use TPR Shadow" },
+	{ 22, "NMI-window Exiting" },
+	{ 23, "MOV-DR Exiting" },
+	{ 24, "Unconditional I/O Exiting" },
+	{ 25, "Use I/O Bitmaps" },
+	{ 27, "Monitor Trap Flag" },
+	{ 28, "Use MSR Bitmaps" },
+	{ 29, "MONITOR Exiting" },
+	{ 30, "PAUSE Exiting" },
+	{ 31, "Activate Secondary Controls" }
+};
+
+/*
+ * Exit capabilities
+ * See SDM volume 3, section 24.7.1
+ */
+struct capability_info exits[5] =
+{
+	{ 2, "Save Debug Controls" },
+	{ 9, "Host Addresss-space Size" },
+	{ 12, "Load IA32_PERF_GLOBAL_CTRL" },
+	{ 15, "Acknowledge Interrupt on Exit" },
+	{ 18, "Save IA32_PAT" },
+	{ 19, "Load IA32_PAT" },
+	{ 20, "Save IA32_EFER" },
+	{ 21, "Load IA32_EFER" },
+	{ 22, "Save VMX-preemption Timer Value" },
+	{ 23, "Clear IA32_BNDCFGS" },
+	{ 24, "Conceal VMX from PT" },
+	{ 25, "Clear IA32_RTIT_CTL" },
+	{ 26, "Clear IA32_LBR_CTL" },
+	{ 28, "Load CET State" },
+	{ 29, "Load PKRS" },
+	{ 30, "Save IA32_PERF_GLOBAL_CTL" },
+	{ 31, "Activate Secondary Controls" }
+};
+
+/*
+ * Entry capabilities
+ * See SDM volume 3, section 24.8.1
+ */
+struct capability_info entries[5] =
+{
+	{ 2, "Load Debug Controls" },
+	{ 9, "IA-32e Mode Guest" },
+	{ 10, "Entry to SMM" },
+	{ 11, "Deactivate dual-monitor treatment" },
+	{ 13, "Load IA32_PERF_GLOBAL_CTRL" },
+	{ 14, "Load IA32_PAT" },
+	{ 15, "Load IA32_EFER" },
+	{ 16, "Load IA32_BNDCFGS" },
+	{ 17, "Conceal VMX from PT" },
+	{ 18, "Load IA32_RTIT_CTL" },
+	{ 20, "Load CET State" },
+	{ 21, "Load Guest IA32_LBR_CTL" },
+	{ 22, "Load Load PKRS" }
 };
 
 /*
@@ -85,6 +166,24 @@ detect_vmx_features(void)
 	pr_info("Pinbased Controls MSR: 0x%llx\n",
 		(uint64_t)(lo | (uint64_t)hi << 32));
 	report_capability(pinbased, 5, lo, hi);
+
+	/* Procbased controls */
+	rdmsr(IA32_VMX_PINBASED_CTLS, lo, hi);
+	pr_info("Procbased Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(procbased, 5, lo, hi);
+
+	/* Exit controls */
+	rdmsr(IA32_VMX_PINBASED_CTLS, lo, hi);
+	pr_info("Exit Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(exits, 5, lo, hi);
+
+	/* Entry controls */
+	rdmsr(IA32_VMX_PINBASED_CTLS, lo, hi);
+	pr_info("Entry Controls MSR: 0x%llx\n",
+		(uint64_t)(lo | (uint64_t)hi << 32));
+	report_capability(entries, 5, lo, hi);
 }
 
 /*
